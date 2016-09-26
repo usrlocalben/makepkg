40e328391b0b4061541461c0a77a70eaf7bd9adb
Dan Dye <ddye@soltra.com>
2016-09-26 16:52:33 -0400

    use pacman license for fpm --license
    defaults to --license='unknown' (same as RPM default)

--- makepkg.sh
+++ makepkg.sh
@@ -2248,12 +2248,6 @@ run_fpm() {
 	else
 		local vendor_param="--vendor \"$RPM_VENDOR\""
 	fi
-	if [[ "$LICENSE" = "" ]]; then
-		warning "RPM will have Soltra EULA License"
-		local license_param=''
-	else
-		local license_param="--license \"$LICENSE\""
-	fi
 	if [[ "$install" = "" ]]; then
 		local after_install_param=''
 	else
@@ -2263,13 +2257,14 @@ run_fpm() {
 	local cmd="fpm -s dir -t rpm --force -a $fpm_arch"
 	cmd="$cmd $rpm_dist_param"
 	cmd="$cmd --rpm-os linux"
+	# cmd="$cmd --debug-workspace"  # skips cleanup of /tmp and allows inspection of SPECFILE
 	cmd="$cmd --rpm-auto-add-directories"
 	cmd="$cmd --package \"$PKGDEST\"" # output path
 	cmd="$cmd $maintainer_param"
 	cmd="$cmd $description_param"
 	cmd="$cmd $url_param"
 	cmd="$cmd $vendor_param"
-	cmd="$cmd $license_param"
+	cmd="$cmd --license \"${license:-unknown}\""
 	cmd="$cmd --name \"$nm\""
 	cmd="$cmd --version \"$pkgver\""
 	cmd="$cmd --iteration \"$pkgrel\""
@@ -2281,6 +2276,7 @@ run_fpm() {
 	done
 	cmd="$cmd --rpm-use-file-permissions --rpm-user root --rpm-group root"
 	cmd="$cmd $@"
+        # echo $cmd  # for debugging
 	eval $cmd
 }
 
